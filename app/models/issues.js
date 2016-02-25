75626bc8ef6beb6a5a549b1e950ded6f5a48b42e
Kamsmm42 <kemal.evans@heig-vd.ch>
2016-02-25 11:43:53 +0100

    ajout de APIDOC

---
+++ app/models/issues.js
@@ -0,0 +1,24 @@
+// Issues model
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var IssuesSchema = new Schema({
+  authorname: 	{ type: String, required: true },
+  date: 		{ type: Date, required: true },
+  description: 	{ type: String, required: true },
+  img_url: 		{ type: String, required: true },
+  tags:  		{ type: String, required: false },
+  email: 		{ type: String, required: false },
+  telephone: 	{ type: Number, required: false },
+  Coord_long: 	{ type: String, required: true },
+  coord_lat: 	{ type: String, required: true }, 
+
+
+  // status
+
+});
+
+
+mongoose.model('Issues', IssuesSchema);
+
